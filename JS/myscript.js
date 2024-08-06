2e8625e81c86abfa49b92860d960e97a93b6fb3a
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2024-08-06 15:34:35 +0530

    JS Day 2
    Basics

---
+++ JS/myscript.js
@@ -0,0 +1,10 @@
+ 
+    let op = document.getElementById("op");
+
+    function displayText(value){
+        if(value.checked){
+            op.style.display = "block";
+        }else{
+            op.style.display = "none";
+        }
+    }
